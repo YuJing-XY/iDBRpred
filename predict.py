0bd0e4942514f973ca56492afa1258e10977160c
YuJing-XY <yujing357@outlook.com>
2026-05-24 01:15:15 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,6 +5,16 @@ import sys
 import time
 import numpy as np
 
+import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(f"Error setting memory growth: {e}")
+
 from src.utils import check_and_download_esm2, parse_fasta
 from src.features import FeatureProcessor
 from src.model import IDRPredictor
